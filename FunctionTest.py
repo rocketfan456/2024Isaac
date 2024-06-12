dffbcd4d6918b56cfde0875c54a209c7e96dffa7
isaacbrej <ibrej@nd.edu>
2024-06-11 20:57:36 -0600

    Creation of Homework 2 files.

---
+++ FunctionTest.py
@@ -0,0 +1,8 @@
+
+import ApogeeRaiseFunc as ff # Replace FunctionFile with the name of your file
+
+
+
+print(ff.ApogeeRaise(1000))  # this should be 2917.652 m/s
+print(ff.ApogeeRaise(5000))  # this should be 2158.525 m/s
+print(ff.ApogeeRaise(10000)) # this should be 1623.149 m/s
