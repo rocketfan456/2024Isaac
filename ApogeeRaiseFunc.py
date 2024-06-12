6b48824ec9a0b28e10023ba9633e7dcc4b547e61
isaacbrej <ibrej@nd.edu>
2024-06-11 21:29:37 -0600

    Adjusted calculations and fixed units

--- ApogeeRaiseFunc.py
+++ ApogeeRaiseFunc.py
@@ -7,16 +7,18 @@ def ApogeeRaise(startingApogee):
     mu = 398600 # km^2/s^2
     periapsis = 185 #km
     rEarth = 6378 # km
-    rTotal = rEarth + startingApogee # km
-    a = ((startingApogee + rEarth)+(periapsis+rEarth))/2 # km
+    rTotal = rEarth + periapsis # km
+    a = ((startingApogee+rEarth)+rTotal)/2 # km
     startingV = np.sqrt(mu*((2/rTotal)-(1/a))) # m/s
 
     # Calculate the needed velocity for new Apogee of 410,000 km
     newApogee = 410000 # km (given)
-    rTotalNew = rEarth + newApogee # km
-    newV = np.sqrt(mu*((2/rTotalNew)-(1/a))) # m/s
+    aNew = ((newApogee+rEarth)+rTotal)/2 # km
+    newV = np.sqrt(mu*((2/rTotal)-(1/aNew))) # m/s
+
+    dV = newV - startingV # km/s
 
     # Find the difference between the two, returning needed dV
-    return newV - startingV
+    return dV*1000 # m/s
 
 
